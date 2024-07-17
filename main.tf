3acf74fc8726d0ed9741031fbe4857cb09d8a310
Inés Tello Marqués <173058694+InesTelloMarques@users.noreply.github.com>
2024-07-17 16:07:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_network_interface" "foo" {
 
 resource "aws_instance" "foo" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = var.instance_name
